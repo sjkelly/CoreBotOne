3d6498d126cf97de2bdcbb487f8d1490538e2911
Steve Kelly <kd2cca@gmail.com>
2013-07-21 15:55:22 -0500

    more mess

--- hbot.py
+++ hbot.py
@@ -5,8 +5,15 @@ from magpie import *
 import numpy
 import copy
 
+
 class XCarriage(element.Primitive):
-    def __init__(self, linearBallBearing="LM8UU", screw="M3", rodSpacing=50, zipTieWidth = 5, zipTieHeight=2):
+    def __init__(self,
+                 linearBallBearing="LM8UU",
+                 screw="M3",
+                 rodSpacing=50,
+                 zipTieWidth=5,
+                 zipTieHeight=2
+                 ):
         element.Primitive.__init__(self, name="xcarriage")
         #Elements used in the design
         self.lb = bearing.LinearBallBearing(size=linearBallBearing)
@@ -15,24 +22,26 @@ class XCarriage(element.Primitive):
         self.lockNut = hardware.LockNut(size=screw)
         self.rodSpacing = rodSpacing
         self.zipTieWidth = zipTieWidth
-        self.lbHolder = LinearBearingHolder(linearBallBearing=self.lb.size, zipTieWidth=self.zipTieWidth)
+        self.lbHolder = LinearBearingHolder(linearBallBearing=self.lb.size,
+                                            zipTieWidth=self.zipTieWidth)
         #Derived parameters
         self.topLength = self.lb.length * 2 + self.lbHolder.wall * 3
         self.bottomLength = self.lb.length + self.lbHolder.wall * 2
         self.plateThick = self.nut.height * 2
-        self.beltClampHoleSpacing = self.rodSpacing - self.lbHolder.width - self.nut.width *2
+        self.beltClampHoleSpacing = self.rodSpacing - self.lbHolder.width - self.nut.width*2
         self.beltClampSpacing = self.lbHolder.length/2
         self.mountingTabRadius = self.lbHolder.length/4-self.zipTieWidth/4
         self.mountingHoles = [
-                             [self.mountingTabRadius, -self.nut.width/2, 0],
-                             [self.topLength/2, -self.nut.width/2, 0],
-                             [self.topLength-self.mountingTabRadius, -self.nut.width/2, 0],
-                             [self.topLength/2-self.mountingTabRadius-self.zipTieWidth/2, self.nut.width/2+self.lbHolder.width+self.rodSpacing, 0],
-                             [self.topLength/2+self.mountingTabRadius+self.zipTieWidth/2, self.nut.width/2+self.lbHolder.width+self.rodSpacing, 0],
+                              [self.mountingTabRadius, -self.nut.width/2, 0],
+                              [self.topLength/2, -self.nut.width/2, 0],
+                              [self.topLength-self.mountingTabRadius, -self.nut.width/2, 0],
+                              [self.topLength/2-self.mountingTabRadius-self.zipTieWidth/2, self.nut.width/2+self.lbHolder.width+self.rodSpacing, 0],
+                              [self.topLength/2+self.mountingTabRadius+self.zipTieWidth/2, self.nut.width/2+self.lbHolder.width+self.rodSpacing, 0]
                              ]
         #element properties
         self.location = [0, 0, 0]
         self.construction = self._construction()
+
     def _construction(self):
         #Construct a plate between the linear bearing holders
         top = element.Cube()
@@ -40,8 +49,10 @@ class XCarriage(element.Primitive):
         top.location = [0, self.lbHolder.width, 0]
         bottom = element.Cube()
         bottom.size = [self.bottomLength, 1, self.plateThick]
-        bottom.location = [self.topLength/2-self.bottomLength/2, self.rodSpacing-1, 0]
-        plate = operation.Hull([top,bottom])
+        bottom.location = [self.topLength/2-self.bottomLength/2,
+                           self.rodSpacing-1,
+                           0]
+        plate = operation.Hull([top, bottom])
 
         #left top bearing holder
         ltBearingHolder = copy.deepcopy(self.lbHolder)
@@ -52,15 +63,19 @@ class XCarriage(element.Primitive):
 
         #bottom center bearing holder
         bcBearingHolder = copy.deepcopy(self.lbHolder)
-        bcBearingHolder.location = [self.topLength/2-self.bottomLength/2, self.rodSpacing, 0]
+        bcBearingHolder.location = [self.topLength/2-self.bottomLength/2,
+                                    self.rodSpacing,
+                                    0]
 
         #bring linear bearings together
-        asm = operation.Union([rtBearingHolder, ltBearingHolder ,bcBearingHolder])
+        asm = operation.Union([rtBearingHolder,
+                               ltBearingHolder,
+                               bcBearingHolder])
         #add plate
         asm += plate
 
         #subtract nuttraps and holes for belt clamps
-        nuts = [copy.deepcopy(nutTrap(self.nut)) for x in range(0,4)]
+        nuts = [copy.deepcopy(nutTrap(self.nut)) for x in range(0, 4)]
         nuts[0].location = [self.topLength/2-self.beltClampSpacing/2,
                             self.lbHolder.width + self.nut.width, 0]
         nuts[1].location = [nuts[0].location[0]+self.beltClampSpacing,
@@ -69,50 +84,48 @@ class XCarriage(element.Primitive):
         nuts[2].location[1] += self.beltClampHoleSpacing
         nuts[3].location = copy.copy(nuts[1].location)
         nuts[3].location[1] += self.beltClampHoleSpacing
-        holes = [copy.deepcopy(element.Hole()) for x in range(0,4)]
+        holes = [copy.deepcopy(element.Hole()) for x in range(0, 4)]
         for idx, nut in enumerate(nuts):
             holes[idx].location = nut.location
             holes[idx].radius = self.nut.diameter/2
             holes[idx].height = self.plateThick + 0.3
         for objs in nuts:
-            objs.location[2] -= 0.1 #shift everything down for clean rendering
+            # shift everything down for clean rendering
+            objs.location[2] -= 0.1
             asm -= objs
         for objs in holes:
-            objs.location[2] -= 0.1 #shift everything down for clean rendering
+            # shift everything down for clean rendering
+            objs.location[2] -= 0.1
             asm -= objs
 
         #add tabs for mounting stuff
         tabs = []
-        for i in range(0,5):
+        for i in range(0, 5):
             tabs.append(dShape(radius=self.mountingTabRadius,
-                                     length=self.plateThick,
-                                     extension=self.mountingTabRadius))
-        #tabNut = nutTrap(self.nut)
-        #tabNut.location = [0, 0, self.plateThick]
-       # tabNut.highlight = True
-        #for i in range(4,5):
-            #
-        for idx,tab in enumerate(tabs):
-            if idx in [3,4]:
-                tab = operation.Rotate(axis=[0, 0, 1], angle=180, elements=[tab])
-            hole = element.Hole(radius=self.screw.outerDiameter/2, height=self.plateThick+0.2)
+                               length=self.plateThick,
+                               extension=self.mountingTabRadius))
+        for idx, tab in enumerate(tabs):
+            if idx in [3, 4]:
+                tab = operation.Rotate(axis=[0, 0, 1],
+                                       angle=180,
+                                       elements=[tab])
+            hole = element.Hole(radius=self.screw.outerDiameter/2,
+                                height=self.plateThick+0.2)
             hole.location = [0, 0, -0.1]
             trap = nutTrap(self.nut)
-            trap.location = [0,0,self.plateThick-self.nut.height]
+            trap.location = [0, 0, self.plateThick-self.nut.height]
             tab -= (hole+trap)
             tab.location = self.mountingHoles[idx]
-            #tab -= copy.deepcopy(tabNut)
+            # tab -= copy.deepcopy(tabNut)
             asm += tab
         return asm
-
-
 
 
 class YCarriage(element.Primitive):
     def __init__(self, linearBallBearing="LM8UU",
                  screw="M3",
                  rodSpacing=50,
-                 zipTieWidth = 5
+                 zipTieWidth=5
                  ):
         element.Primitive.__init__(self, name="xcarriage")
         self.lb = bearing.LinearBallBearing(size=linearBallBearing)
@@ -130,7 +143,6 @@ class YCarriage(element.Primitive):
         return asm
 
 
-
 class MotorMount(element.Primitive):
     def __init__(self, stepper="GenericNEMA17", screw="M3", rodDiameter=8, mountLength=38):
         element.Primitive.__init__(self, name="motormount")
@@ -142,29 +154,153 @@ class MotorMount(element.Primitive):
         #element properties
         self.location = [0, 0, 0]
         self.construction = self._construction()
+
     def _construction(self):
         pass
 
-class YRodMount(component.Element):
-    def __init__(self, rodDiameter=8, mountLength=38, stepper="GenericNEMA17", belt="GT2", beltSeperation=16, beltWidth=6, holeDiameter=3.5):
+
+class YBearingMount(component.Element):
+    def __init__(self, rodDiameter=8,
+                 mountLength=38,
+                 stepper="GenericNEMA17",
+                 beltSize="GT2",
+                 bearingSize="625zz",
+                 beltWidth=6,
+                 holeDiameter=3.5):
         component.Element.__init__(self, name="yrodmount")
         #Used Elements
         self.stepper = motor.Stepper(size=stepper)
-        self.belt = belt.TimingBelt(size=belt)
-        
+        self.belt = belt.TimingBelt(size=beltSize)
+        self.bearing = bearing.BallBearing(size=bearingSize)
+        self.beltSeperation = self.bearing.outerDiameter
+        self.yRodMount = YRodMount(rodDiameter=rodDiameter, mountLength=mountLength, stepper=stepper, beltSize=beltSize,
+                                   beltSeperation=self.beltSeperation, beltWidth=beltWidth, holeDiameter=holeDiameter)
+        self.bearingHoldRadius = self.bearing.outerDiameter/2 + self.belt.height + 2
+        self.bearingHoldHeight = (self.yRodMount.height - self.yRodMount.beltPass[2])/2
+        self.construction = self._construction()
+
+    def _construction(self):
+        asm = self.yRodMount
+#        upperBearingHold = dShape(radius=self.bearingHoldRadius,
+#                                  extension=self.bearingHoldRadius,
+#                                  length=self.bearingHoldHeight)
+#        lowerBearingHold = copy.deepcopy(upperBearingHold)
+#        upperBearingHold.location = [self.yRodMount.width/2,
+#                                     -self.bearing.outerDiameter/2-1,
+#                                     self.yRodMount.beltPass[2]+self.bearingHoldHeight]
+#        lowerBearingHold.location = [self.yRodMount.width/2,
+#                                     -self.bearing.outerDiameter/2-1,
+#                                     0]
+#        hole = element.Hole(radius=self.bearing.innerDiameter/2,
+#                            height=self.yRodMount.height+0.2)
+#        hole.location = copy.copy(lowerBearingHold.location)
+#        hole.location[2] = -0.1
+#        asm = operation.Union([asm, upperBearingHold, lowerBearingHold]) - hole
+        return asm
+
+
+class BeltClamp(component.Element):
+    def __init__(self, thickness=5, screw="M3", xCarriage=None):
+        component.Element.__init__(self, name="yrodmount")
+        self.holeSpacing = xCarriage.beltClampHoleSpacing
+        self.screw = hardware.Nut(size=screw)
+        self.thickness = thickness
+        self.radius = self.width
+    def _construction(self):
+        pass
+
+
+class YRodMount(component.Element):
+    def __init__(self,
+                 rodDiameter=8,
+                 mountLength=38,
+                 stepper="GenericNEMA17",
+                 beltSize="GT2",
+                 nutSize="M5",
+                 linearBearing = "LM8UU",
+                 beltSeperation=16,
+                 beltWidth=6,
+                 holeDiameter=3.5):
+        component.Element.__init__(self, name="yrodmount")
+        #Used Elements
+        self.stepper = motor.Stepper(size=stepper)
+        self.belt = belt.TimingBelt(size=beltSize)
+        self.nut = hardware.Nut(size=nutSize)
+        self.lb = bearing.LinearBallBearing(size=linearBearing)
+        self.beltSeperation = beltSeperation
+        self.beltWidth = beltWidth
+        self.holeDiameter = holeDiameter
+        self.beltWidth = beltWidth
+        self.plateThick = self.nut.height*2
+        self.length = mountLength
+        self.width = self.stepper.width
+        self.height = rodDiameter*2.5
+        self.rodEncasementDiameter = self.lb.outerDiameter
+        self.rodDiameter = rodDiameter
+        self.rodCenter = self.plateThick + self.rodDiameter/2
+        #should be reassigned by implementation
+        self.rodDepth = self.length * 0.8
+        #should be reassigned by implementation
+        self.rodStart = self.length - self.rodDepth
+        # beltpass represents space around the belt
+        self.beltPass = [self.belt.height*3, self.width+0.2, self.beltWidth*2]
+        # setup hole locations
+        leftX = self.width/2-self.beltSeperation/2-self.beltPass[0]-self.holeDiameter/2
+        rightX = self.width/2+self.beltSeperation/2+self.beltPass[0]+self.holeDiameter/2
+        self.mountingHoles = [[leftX, self.length*0.25, -0.1],
+                              [leftX, self.length*0.75, -0.1],
+                              [rightX, self.length*0.25, -0.1],
+                              [rightX, self.length*0.75, -0.1]
+                             ]
+        #tab radius
+        self.tabRadius = leftX
+        self.construction = self._construction()
+                              
+    def _construction(self):
+        #asm = element.Cube(size=[self.width, self.length, self.plateThick])
+        #rod encasement
+        rodEn = dShape(radius=self.rodEncasementDiameter/2,
+                                 extension=self.rodDiameter/2+self.plateThick,
+                                 length=self.length)
+        rodEn = operation.Rotate(axis=[1, 0, 0], angle=-90, elements=[rodEn])
+        rodEn.location = [self.width/2, 0, self.rodCenter]
+        asm = rodEn
+        #rod
+        rod = element.Hole(radius=self.rodDiameter/2, height=self.length)
+        rod.rotation.axis = [1, 0, 0]
+        rod.rotation.angle = -90
+        rod.location = [self.width/2, self.rodStart, self.rodCenter]
+        asm -= rod
+        #Mounting holes
+        for i in range(0, 4):
+            holeTab = dShape(radius=self.tabRadius,
+                             extension=self.width/2,
+                             length=self.plateThick)
+            holeTab = operation.Translate(location=self.mountingHoles[i],
+                                          elements=[holeTab])
+            hole = element.Hole(radius=self.holeDiameter/2,
+                                height=self.plateThick+0.2)
+            hole.location = self.mountingHoles[i]
+            asm = asm + holeTab - hole
+        return asm
+
 
 class LinearBearingHolder(component.Element):
-
-    def __init__(self, linearBallBearing="LM8UU", zipTieWidth = 5, zipTieHeight=2):
+    def __init__(self,
+                 linearBallBearing="LM8UU",
+                 zipTieWidth=5,
+                 zipTieHeight=2
+                 ):
         component.Element.__init__(self, name="linearbearingholder")
-        self.lb = bearing.LinearBallBearing(size=linearBallBearing, negative=True)
+        self.lb = bearing.LinearBallBearing(size=linearBallBearing,
+                                            negative=True)
         self.zipTieWidth = zipTieWidth
         self.location = [0, 0, 0]
         self.wall = self.lb.outerDiameter*0.25
         self.length = self.lb.length + self.wall * 2
         self.width = self.lb.outerDiameter + self.wall * 2
         self.bearingCenter = self.width/2 + zipTieHeight
-        self.height = self.lb.outerDiameter * 0.60 + self.wall + zipTieHeight
+        self.height = self.lb.outerDiameter * 0.70 + self.wall + zipTieHeight
         self.construction = self._construction()
 
     def _construction(self):
@@ -173,16 +309,22 @@ class LinearBearingHolder(component.Element):
         core.rotation.angle = 90
         core.location = [0, self.width/2, self.bearingCenter]
 
-        squareBase1 = element.Cube(size=[(self.length-self.zipTieWidth)/2, self.width, self.bearingCenter])
+        squareBase1 = element.Cube(size=[(self.length-self.zipTieWidth)/2,
+                                         self.width,
+                                         self.bearingCenter])
         squareBase2 = copy.deepcopy(squareBase1)
 
         squareBase2.location = [(self.length+self.zipTieWidth)/2, 0, 0]
 
-        rodClearance = dShapeNeg(radius = self.lb.innerDiameter/2+0.5,
+        rodClearance = dShapeNeg(radius=self.lb.innerDiameter/2+0.5,
                                  extension=self.lb.outerDiameter,
                                  length=self.length+0.2)
-        rodClearance = operation.Rotate(axis=[0, 0, 1], angle=90, elements=[rodClearance])
-        rodClearance = operation.Rotate(axis=[0, 1, 0], angle=90, elements=[rodClearance])
+        rodClearance = operation.Rotate(axis=[0, 0, 1],
+                                        angle=90,
+                                        elements=[rodClearance])
+        rodClearance = operation.Rotate(axis=[0, 1, 0],
+                                        angle=90,
+                                        elements=[rodClearance])
         rodClearance.location = [-0.1, self.width/2, self.bearingCenter]
         lb = self.lb
         lb.rotation.axis = [0, 1, 0]
@@ -193,6 +335,7 @@ class LinearBearingHolder(component.Element):
         asm = operation.Intersection([asm, keep])
         return asm
 
+
 def dShapeNeg(radius, extension, length):
     """Makes a D shape to subtract from linear bearing holders"""
     a = element.Hole(radius=radius, height=length)
@@ -200,17 +343,20 @@ def dShapeNeg(radius, extension, length):
     b.location = [-radius, 0, 0]
     return a + b
 
+
 def dShape(radius, extension, length):
     """Makes a D shape to subtract from linear bearing holders"""
     a = element.Cylinder(radius=radius, height=length)
     b = element.Cube(size=[radius*2, extension, length])
     b.location = [-radius, 0, 0]
-    return a + b
+    return copy.deepcopy(a + b)
+
 
 def nutTrap(nut):
     return element.Ntube(apothem=nut.width/2, sides=6, height=nut.height+0.1)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     #Config
     stepper = "GenericNEMA17"
     linearBearing = "LM8UU"
@@ -218,11 +364,17 @@ if __name__=="__main__":
     rodSpacing = 45
     zipTieWidth = 5
     zipTieHeight = 2
-    #ytest = 
+    ybearing = YBearingMount(rodDiameter=8,
+                             mountLength=38,
+                             stepper=stepper,
+                             beltSize="GT2",
+                             bearingSize="625zz",
+                             beltWidth=6,
+                             holeDiameter=3.5)
     xcar = XCarriage(linearBallBearing=linearBearing,
                      screw=screw,
                      rodSpacing=rodSpacing,
                      zipTieWidth=zipTieWidth,
-                     zipTieHeight=zipTieHeight
-                    )
+                     zipTieHeight=zipTieHeight)
     utility.export(xcar, "./json/xcar.json")
+    utility.export(ybearing, "./json/ybearing.json")
